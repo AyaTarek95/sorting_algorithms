248347b5efc9f6b80a5c808fcbd9a9f82f48bc8a
AyaTarek95 <aya.tarek213@icloud.com>
2023-10-19 02:50:20 -0700

    task2

---
+++ 2-selection_sort.c
@@ -0,0 +1,30 @@
+#include "sort.h"
+/**
+ * selection_sort- sorts an array of integers
+ * @array: to sort
+ * :size: array's size
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t x, y, z;
+	int tmp;
+
+	if (!array || !size)
+		return;
+
+	for (x = 0; x < size - 1; x++)
+	{
+		for (y = size - 1, z = x + 1; y > x; j--)
+		{
+			if (array[y] < array[z])
+				z = y;
+		}
+		if (array[x] > array[z])
+		{
+			tmp = array[x];
+			array[x] = array[z];
+			array[z] = tmp;
+			print_array(array, size);
+		}
+	}
+}
