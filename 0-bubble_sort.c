7b57a17453df0ba88da9e78ed44efef43f92e4c1
AyaTarek95 <aya.tarek213@icloud.com>
2023-10-24 09:07:51 -0700

    fix 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,26 +7,26 @@
 void bubble_sort(int *array, size_t size)
 {
 	int tmp;
-	size_t x, y;
+	size_t x, z;
 
 	if (!array || !size)
 		return;
 
 	x = 0;
 
-	while (i < size)
+	while (x < size)
 	{
 		for (z = 0; z < size - 1; z++)
 		{
-			if (array[k] > array[k + 1])
+			if (array[z] > array[z + 1])
 			{
-				tmp = array[k];
-				array[k] = array[k + 1];
-				array[k + 1] = tmp;
+				tmp = array[z];
+				array[z] = array[z + 1];
+				array[z + 1] = tmp;
 				print_array(array, size);
 			}
 		}
-		i++;
+		z++;
 	}
 
 }
