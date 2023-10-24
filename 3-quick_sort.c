4ac8e4bd3a2838c7ab194b66226b098317bbf8b7
AyaTarek95 <aya.tarek213@icloud.com>
2023-10-24 10:53:48 -0700

    fix 3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,20 +1,18 @@
 #include "sort.h"
 /**
  * My_SwapII- swap 2 ints
- * @array: to swap in
  * @size: of array
  * @x:address of 1st value
  * @y: address of 2nd value
  */
 void My_SwapII(int *array, size_t size, int *x, int *y)
 {
-		if (x != y)
-		{
-			*x = *x + *y;
-			*y = *x - *y;
-			*x = *x - *y;
-		}
-	print_array((const int *)array, size);
+	int tmp = *x;
+
+	*x = *y;
+	*y = tmp;
+
+print_array((const int *)array, size);
 }
 
 /**
@@ -27,38 +25,39 @@ void My_SwapII(int *array, size_t size, int *x, int *y)
  */
 size_t Lomuto_part(int *array, size_t size, size_t low, size_t high)
 {
-	int x, y, piv;
+	int x, z, pivot_value = array[high];
 
-	piv = array[high];
-
+	x = z = low;
 	for (x = z = low; z < high; z++)
-
-		if (array[z] < piv)
+	{
+		if (array[z] < pivot_value)
 			My_SwapII(array, size, &array[z], &array[x++]);
-	My_SwapII(array, size, &array[x], array[high]);
+		
+	}
+	My_SwapII(array, size, &array[x], &array[high]);
 return (x);
 }
 
 /**
- * QuickSort- sort via Lomuto partition scheme
+ * QuickSort_recursion- sort via Lomuto partition
  * @array: to sort
- * @size: of arrray
+ * @size: size of array
  * @low: lowest point to start
  * @high: size-1
  */
-void QuickSort(int *array, size_t size, size_t low, size_t high)
+void QuickSort_recursion(int *array, size_t size,  size_t low, size_t high)
 {
 	if (low < high)
 	{
-		size_t P = Lomuto_part(array, size, low, high);
+		size_t P_index = Lomuto_part(array, size, low, high);
 
-		QuickSort(array, size, low, P - 1);
-		QuickSort(array, size, P + 1, high);
+		QuickSort_recursion(array, size, low, P_index - 1);
+		QuickSort_recursion(array, size, P_index + 1, high);
 	}
 }
 
 /**
- * quick_sort- sorts an array of integers
+ * quick_sort- sort array of ints
  * @array: to sort
  * @size: of array
  */
@@ -66,5 +65,5 @@ void quick_sort(int *array, size_t size)
 {
 	if (!array || !size)
 		return;
-	QuickSort(array, size, 0, size - 1);
+	QuickSort_recursion(array, size, 0, size - 1);
 }
--- sort.h
+++ sort.h
@@ -3,9 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
-void bubble_sort(int *array, size_t size);
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -19,15 +17,15 @@ typedef struct listint_s
 	struct listint_s *prev;
 	struct listint_s *next;
 } listint_t;
-
+void bubble_sort(int *array, size_t size);
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 void my_swap(listint_t *i, listint_t *k);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-size_t Lomuto_part(int *array, size_t size, size_t low, size_t high);
-void QuickSort(int *array, size_t size, size_t low, size_t high);
+size_t Lomuto_part(int *array,size_t size, size_t low, size_t high);
+void QuickSort_recursion(int *array, size_t size, size_t low, size_t high);
 void My_SwapII(int *array, size_t size, int *x, int *y);
 
 #endif
